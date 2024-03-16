0ca29288354dc359f6a3a40681f85f2f4549e79f
jd.gupta27 <jd.gupta27@gmail.com>
2024-03-16 22:28:18 +0530

    first commit

---
+++ src/main/java/com/jd/SprintBootProjectWithJenkins/SprintBootProjectWithJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jd.SprintBootProjectWithJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprintBootProjectWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprintBootProjectWithJenkinsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/SprintBootProjectWithJenkins/SprintBootProjectWithJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.SprintBootProjectWithJenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootProjectWithJenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
