a455821a1cbe38e202d7c5d5c23c3985d096b816
jd.gupta27 <jd.gupta27@gmail.com>
2024-03-22 22:55:24 +0530

    commit

--- src/main/java/com/jd/SprintBootProjectWithJenkins/SprintBootProjectWithJenkinsApplication.java
+++ src/main/java/com/jd/SprintBootProjectWithJenkins/SprintBootProjectWithJenkinsApplication.java
@@ -8,6 +8,7 @@ public class SprintBootProjectWithJenkinsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SprintBootProjectWithJenkinsApplication.class, args);
+		System.out.println("Hello");
 	}
 
 }
